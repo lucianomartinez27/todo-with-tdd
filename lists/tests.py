b8411928f5518ca0183e70600444772c546ec04a
Luciano Martinez <lucianomartinez27@gmail.com>
2020-11-15 00:31:03 -0300

    added Item to model and respective Test

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
-from django.http import HttpRequest
 from django.test import TestCase
 from .views import home_page
+from .models import Item
 from django.urls import resolve
 # Create your tests here.
 
@@ -24,3 +24,21 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'index.html')
+
+class ModelItemTest(TestCase):
+    def test_saving_and_retriving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) item list'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEquals(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) item list')
+        self.assertEqual(second_saved_item.text, 'The second item')
