e8df5b4b773f5454b05ce346c5542e32190a20bb
Luciano Martinez <lucianomartinez27@gmail.com>
2020-11-14 23:20:02 -0300

    refactored functional tests methods

--- functional_test.py
+++ functional_test.py
@@ -7,41 +7,41 @@ class VisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        # I open to the browser and go to my TO-DO website
+        self.browser.get("http://localhost:8000")
     
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # I open to the browser and go to my TO-DO website
-        self.browser.get("http://localhost:8000")
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
-        # I check if the web si correct checking the Titles
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name("h1").text
-        self.assertIn('To-Do', header_text)
-        # Im searching for an input label
+    def add_todo(self, todo_to_add):
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys(todo_to_add)
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+    def test_the_input_box_is_showed_correctly(self):
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             input_box.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-        # ...and insert my First TO-DO
-        input_box.send_keys('Buy a peacock feathers')
-        input_box.send_keys(Keys.ENTER)
-        time.sleep(2)
-        # i want add a one more TO-DO
-        input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('Buy a new brain')
-        input_box.send_keys(Keys.ENTER)
-        time.sleep(2)
-        # checking if the TO-DOs were correcly added
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy a peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Buy a new brain', [row.text for row in rows])
 
-        self.fail('Test finished is not')
-        #[.. rest of comment as before]
+    def test_the_title_is_the_corresponding(self):
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn('To-Do', header_text)
+
+    def test_todo_can_be_added_correctly(self):
+        self.add_todo('Buy a peacock feathers')
+        self.add_todo('Buy a new brain')
+        # checking if the TO-DOs were correcly added
+        self.check_for_row_in_list_table('1: Buy a peacock feathers')
+        self.check_for_row_in_list_table('1: Buy a new brain')
 
 
 if __name__ == '__main__':
