dcf60e5a3d7d7be1597ee1dd5d026a64bf2cadab
Luciano Martinez <lucianomartinez27@gmail.com>
2020-11-17 22:03:52 -0300

    simplified home

--- lists/views.py
+++ lists/views.py
@@ -9,8 +9,7 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
-    items = Item.objects.all()
-    return render(request, 'index.html', {'items': items})
+    return render(request, 'index.html')
 
 def view_list(request):
     items = Item.objects.all()
