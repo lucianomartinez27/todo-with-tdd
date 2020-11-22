56f6138869888fb98a38be15280a25ef0a9d2bbc
Luciano Martinez <lucianomartinez27@gmail.com>
2020-11-22 14:14:34 -0300

    using get_absolute_url on List model

--- lists/urls.py
+++ lists/urls.py
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+
 from django.urls import path
-from lists.views import home_page
 from lists.views import view_list
 from lists.views import new_list
 
--- lists/views.py
+++ lists/views.py
@@ -18,7 +18,7 @@ def view_list(request, list_id):
             item = Item.objects.create(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect(f'/lists/{list_.id}/')
+            return redirect(list_)
         except ValidationError:
             item.delete()
             error = "You can't have an empty list"
@@ -35,4 +35,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list"
         return render(request, 'index.html', {'error': error})
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(list_)
